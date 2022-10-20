70241fa3feff41c81fd55131bac3ea2e2f309e1b
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2022-10-20 00:48:58 -0500

    Agregar un usuario simple, login simple del usuario, agregar una tarea, detalle de la tarea

--- Proyecto_cloud/flaskr/app.py
+++ Proyecto_cloud/flaskr/app.py
@@ -1,7 +1,10 @@
 from flaskr import create_app
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+
+
 from .modelos import db
-from .vistas import LoadAudio,DownloadAudio
+from .vistas import LoadAudio,DownloadAudio, TaskDetail, CreateTask, CreateUser, LoginUser
 
 app = create_app('default')
 app_context = app.app_context()
@@ -14,3 +17,9 @@ db.create_all()
 
 api.add_resource(LoadAudio, '/api/tasks')
 api.add_resource(DownloadAudio, '/api/files/<string:filename>')
+api.add_resource(TaskDetail, '/api/tasks/<int:id_task>')
+api.add_resource(CreateTask, '/api/tasks/<int:id_usuario>/create')
+api.add_resource(CreateUser, '/api/user/create')
+api.add_resource(LoginUser, '/api/user/login')
+
+jwt = JWTManager(app)
--- Proyecto_cloud/flaskr/vistas/vistas.py
+++ Proyecto_cloud/flaskr/vistas/vistas.py
@@ -1,12 +1,16 @@
+from sqlalchemy.exc import IntegrityError
 from flask_restful import Resource
 from flask import request,Flask, request, send_from_directory
+from flask_jwt_extended import jwt_required, create_access_token,get_jwt
 from datetime import datetime
-from ..modelos import db, Task
+from ..modelos import db, Task, Usuario, TaskSchema
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOADS_FOLDER'] = 'uploads/audios/'
+
+task_schema = TaskSchema()
 
 class DownloadAudio(Resource):
 
@@ -38,4 +42,56 @@ class LoadAudio(Resource):
       else:
          return {"mensaje": "formato no valido a transformar"}
 
+class TaskDetail(Resource):
 
+   #  @jwt_required()
+    def get(self, id_task):
+        return task_schema.dump(Task.query.get_or_404(id_task))
+
+class CreateTask(Resource):
+
+    def post(self, id_usuario):
+      new_task = Task(filename=request.json["filename"],
+                  initialformat= request.json["initialformat"],
+                  path= request.json["path"],
+                  newformat= request.json["newformat"],
+                  timestamp= request.json["timestamp"],
+                  state= request.json["state"],)
+      usuario = Usuario.query.get_or_404(id_usuario)
+      usuario.tasks.append(new_task)
+
+      try:
+            db.session.commit()
+      except IntegrityError:
+            db.session.rollback()
+            return 'El usuario ya tiene un carrera con dicho nombre', 409
+
+      return task_schema.dump(new_task)
+
+class LoginUser(Resource):
+
+   def post(self):
+      usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
+                                    Usuario.Password1 == request.json["Password1"]).first()
+      db.session.commit()
+      if usuario is None:
+         return "El usuario no existe", 404
+      else:
+         token_de_acceso = create_access_token(identity=usuario.id)
+         return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
+
+class CreateUser(Resource):
+   def post(self):
+        Password1 = request.json["Password1"]
+        if (len(Password1)>8):
+            usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"]).first()
+            if usuario is None:
+                nuevo_usuario = Usuario(usuario=request.json["usuario"], Password1=request.json["Password1"], Password2 = request.json["Password1"], correo= request.json["correo"])
+                db.session.add(nuevo_usuario)
+                db.session.commit()
+                token_de_acceso = create_access_token(identity=nuevo_usuario.id)
+                return {"mensaje": "usuario creado exitosamente", "token": token_de_acceso, "id": nuevo_usuario.id}
+            else: return {"mensaje": "El usuario ya existe en la plataforma"} , 409
+
+        else:
+            return {"mensaje": "La contraseña debe de tener más de 8 caracteres"}, 409
