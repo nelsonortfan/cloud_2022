26f576391e78678eb5e2586f7101d34b7d0864be
DaMoAndes <d.moralesa2@uniandes.edu.co>
2022-10-23 19:44:09 -0500

    List Task and Delete Tasks

--- Proyecto_cloud/flaskr/app.py
+++ Proyecto_cloud/flaskr/app.py
@@ -1,7 +1,8 @@
 from flaskr import create_app
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 from .modelos import db
-from .vistas import LoadAudio,DownloadAudio, ListAllTask
+from .vistas import LoadAudio,DownloadAudio, ListAllTask,TaskDetail,VistaSignIn,VistaLogIn, DeleteAudio, VistaUpdateTask
 
 app = create_app('default')
 app_context = app.app_context()
@@ -12,6 +13,15 @@ api.init_app(app)
 db.init_app(app)
 db.create_all()
 
-api.add_resource(LoadAudio, '/api/tasks')
 api.add_resource(DownloadAudio, '/api/files/<string:filename>')
-api.add_resource(ListAllTask, '/api/show_tasks')
+api.add_resource(LoadAudio, '/api/tasks')
+api.add_resource(DeleteAudio, '/api/delete/')
+api.add_resource(ListAllTask, '/api/<int:id_usuario>/show_tasks')
+api.add_resource(VistaUpdateTask, '/api/update_tasks/<int:id_task>')
+api.add_resource(VistaSignIn, '/api/auth/signup')
+api.add_resource(VistaLogIn, '/api/user/login')
+
+
+
+
+jwt = JWTManager(app)
