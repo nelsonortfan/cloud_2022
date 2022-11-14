dbf233b9995c0d09f4aa5bd05a89d2718d55d581
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-11-14 10:13:43 -0500

    Update connection with storage for dowload file

--- Proyecto_cloud/flaskr/vistas/vistas.py
+++ Proyecto_cloud/flaskr/vistas/vistas.py
@@ -56,14 +56,15 @@ class DownloadAudio(Resource):
             storage_client = storage.Client()
             bucket = storage_client.bucket(bucket_name)
             blob = bucket.blob(source_blob_name)
-            contents = blob.download_as_string()
-            # blob.download_to_filename(destination_file_name)
+            blob.download_to_filename('../uploads/audios/' + destination_file_name)
 
             print(
-               "Downloaded storage object {} from bucket {} as the following string: {}.".format(
-                     source_blob_name, bucket_name, contents
+               "Downloaded storage object {} from bucket {} to local file {}.".format(
+                     source_blob_name, bucket_name, destination_file_name
                )
             )
+
+            return send_from_directory('../uploads/audios/', filename)
 
       except Exception as e:
          return {"mensaje": "Archivo {} no existe".format(filename)},404 
