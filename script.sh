d6ea9bc18427b5049aed966d117b67acefa4c16f
nelsonortfan <n.ortizf@uniandes.edu.co>
2022-11-14 20:45:36 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
- cd /home/d_moralesa2/cloud_2022/Proyecto_cloud/flaskr && flask run -h 0.0.0.0
+#cd /home/d_moralesa2/cloud_2022/Proyecto_cloud/flaskr && flask run -h 0.0.0.0
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+* * * * * echo "App Iniciada" >> /home/jhonsbg/log.txt
+@reboot /home/d_moralesa2/cloud_2022/script.sh
