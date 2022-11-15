007cddba30fe7d31b996a8222d4bb282a19e6cf0
DB20 <kd.barrera@uniandes.edu.co>
2022-11-14 20:13:11 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-pip install google
-sh /home/d_moralesa2/cloud_2022/Proyecto_cloud/flaskr 
-flask run -h 0.0.0.0
+ cd /home/d_moralesa2/cloud_2022/Proyecto_cloud/flaskr && flask run -h 0.0.0.0
